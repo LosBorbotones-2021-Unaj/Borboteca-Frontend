8487b586b468f3b17966a232f8aff75681e2957b
TilFerLix <Fernando.blanco004@gmail.com>
2021-11-01 17:25:38 -0300

    VistaLibro3

--- js/codigo.js
+++ js/codigo.js
@@ -4,9 +4,16 @@ const gc3 = document.querySelector('.content__main_grid #gridChild3 .libroPrecio
 
 const url = 'https://localhost:44331/api/Libro?id=';
 const url2 = 'https://localhost:44381/api/CarroLibro';
+const url3 = 'https://localhost:44381/api/Ventas?UsuarioId=';
+
+function obtenerLocalStorage(){
+    let userId = localStorage.getItem("usuarioId");
+    let idLibro = localStorage.getItem("libroId");
+}
+
+InfoLibro(idLibro);
 
 function InfoLibro(idLibro){
-    //Libro
     fetch(url + idLibro)
     .then(response => response.json())
     .then(response => {
@@ -34,19 +41,27 @@ var boton = document.getElementById('btn');
 boton.addEventListener("click", clicked);
 
 function clicked(){
-    
+    fetch(url3 + userId, {
+        method : "POST",
+        headers: {"Content-type" : "application/json"}
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => console.log(err));
+
     fetch(url2, {
         method : "POST",
         body : JSON.stringify({
-            "libroid" : "55",
-            "carroid" : "2"
+            "libroid" : idLibro,
+            "usuarioId" : userId
         }),
         headers: {"Content-type" : "application/json"}
     })
     .then(response => response.json())
     .then(response => {
         console.log(response);
-        
     })
     .catch(err => console.log(err));
 
