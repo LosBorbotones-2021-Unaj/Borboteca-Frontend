8d03bd99335a46a1e16312f84e5488656ab8f4d3
carlos-j-98 <carlossuarez235@gmail.com>
2021-12-01 23:49:11 -0300

    Descargas

--- js/services/libros-index.js
+++ js/services/libros-index.js
@@ -1,5 +1,7 @@
-const urlBase = "https://localhost:44331/api/Libro/";
+import fileDownload from 'js-file-download';
 
+const urlBase = "https://localhost:44331/api/Libro/";
+const urlBase2 = "https://localhost:44331/api/Libro";
 export const pedirLibros = (indice,callback) => {
 
     fetch(urlBase + `PedirLibros/${indice}`)
@@ -17,4 +19,21 @@ export const pedirPaginas = () => {
         if(httpResponse.ok)
             return httpResponse.json();
     })
+}
+
+export const DescargarLibro = () => {
+    fetch ("https://localhost:44331/api/Libro?Guid_Id=2550e1b1-9e48-43b4-875f-14634d4c07f1",{
+        responseType : 'blob',
+        method: 'GET',
+        headers: new Headers({
+          'Authorization': `Bearer ${token}`, 
+          'Content-Type': 'application/json'
+        })
+    })
+    .then((httpResponse) => {
+        return httpResponse.blob();
+    })
+    .then((data) =>{
+        fileDownload(data, 'filename.pdf');
+    });
 }
